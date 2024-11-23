4564bb3156c062a2b0b586524791cf1db0e3f0f6
aronmin <scm0418@naver.com>
2024-11-23 14:32:45 +0900

    work

--- src/main/java/com/ll/ch/doamin/chat/chatRoom/controller/ApiV1ChatRoomControllers.java
+++ src/main/java/com/ll/ch/doamin/chat/chatRoom/controller/ApiV1ChatRoomControllers.java
@@ -4,11 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController // 외부에서 오는 http와 상호작용 가능
 @RequestMapping("/api/vq/chat/rooms")
 public class ApiV1ChatRoomControllers {
     @GetMapping
-    public String getChatRooms(){
-        return "채팅방 목록";
+    public List<String> getChatRooms(){
+        return List.of(
+                "room1",
+                "room2",
+                "room3"
+        );
     }
 }
